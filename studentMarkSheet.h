133ab2fc8a2d5aba701f4cdc6e903f5b74b45496
SHAIK ALMAS <almasshaik045@gmail.com>
2025-08-01 12:43:18 +0530

    Update studentMarkSheet.h

--- studentMarkSheet.h
+++ studentMarkSheet.h
@@ -2,6 +2,7 @@
 #define __studentMarkSheet__
 #include<stdio.h>
 #include <stdlib.h>
+
 typedef struct studentMarkSheet{
         char firstName[50];
         char lastName[50];
@@ -14,6 +15,7 @@ typedef struct studentMarkSheet{
 
 typedef struct classRecord{
         struct studentMarkSheet *students;
+        char className[30];
         int noOfStudents;
         int noOfStuPassed;
         float avgOfStudentsPassed;
@@ -26,6 +28,10 @@ void findStudent(ClassRecord classRecord);
 int deleteStudentRecord(int rollNo);
 void exportStudentReport(ClassRecord classRecord);
 int addStudent(ClassRecord *classRecord,FILE *studentFile);
+int stringValidate(char *p);
+int numberValidate(int num);
+int validateMarks(int num);
+void viewClassReport(ClassRecord *classRecord);
 
 
 #endif
